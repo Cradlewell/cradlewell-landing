8729d3351b2ae70a3aab539fb6df15b5574b97c1
Cradlewell <care@cradlewell.com>
2025-07-22 16:06:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/images/favicon.png" />
         {/* Font Awesome CDN */}
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
