f80789c10e5cb275ab7d2257190f3df9b5f7b5fb
Cradlewell <care@cradlewell.com>
2026-05-07 17:34:41 +0530

    Update HeroHeaderShape.tsx

--- src/components/HeroHeaderShape.tsx
+++ src/components/HeroHeaderShape.tsx
@@ -121,7 +121,7 @@ const HeroHeader1 = () => {
           <Col lg={6} className="mt-5 mt-lg-0">
             <div className="hero-image-wrap">
               <img
-                src="/images/bannerimg.png"
+                src="/images/website_hero_image.png"
                 alt="Trusted Nurse-led Newborn and Postnatal Home Care Support Banner for Mothers and Babies by Cradlewell in Bangalore"
                 className="rounded img-fluid mt-3"
                 fetchPriority="high"
